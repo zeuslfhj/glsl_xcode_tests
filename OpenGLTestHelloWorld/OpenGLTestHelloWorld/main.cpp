a15118f0a490dc9fecae9af21ad0a8d52752fd33
zero wilson <zeuslfhj@hotmail.com>
2014-12-15 12:11:27 +0800

    Add OpenGLTest and OpenGLHelloWorld

---
+++ OpenGLTestHelloWorld/OpenGLTestHelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLTestHelloWorld
+//
+//  Created by zero wilson on 15/12/14.
+//  Copyright (c) 2014 zero wilson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
